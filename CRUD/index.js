8a4bfe38aee0774fcac97416d8a0c650131dfd04
ak8459 <87300147+ak8459@users.noreply.github.com>
2023-10-29 12:09:33 +0530

    new commits

--- CRUD/index.js
+++ CRUD/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { connection } = require('./database/db');
 const { userRouter } = require('./routes/user.route');
 require('dotenv').config()
+const cors = require('cors');
 const { noteRouter } = require('./routes/notes.route')
 let port = process.env.PORT;
 const app = express();
 app.use(express.json());
 
-
+app.use(cors())
 
 app.use('/user', userRouter);
 app.use('/notes', noteRouter);
